ecbaf5fdf5e62c6c0074f77b4e66194c81f78dbe
unknown <habibullayevfarzot04@gmail.com>
2025-05-04 12:51:04 +0500

    Port is changed

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ import { config } from 'dotenv';
 async function bootstrap() {
   config(); // .env fayl uchun
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-  console.log(`Server is running in 3000`)
+  await app.listen(4444);
+  console.log(`Server is running in 4444`);
   console.log('BOT_TOKEN:', process.env.BOT_TOKEN);
 
 }
